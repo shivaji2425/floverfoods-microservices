f2789c8a0b750b66eea996adec5a75ffb8ca8889
shivaji2425 <kirlashivaji121@gmail.com>
2025-11-05 12:21:28 +0530

    changes done in the OrderItemRepository

--- OrderManagement/src/main/java/com/microservices/dao/OrderItemRepository.java
+++ OrderManagement/src/main/java/com/microservices/dao/OrderItemRepository.java
@@ -1,5 +1,11 @@
 package com.microservices.dao;
 
-public interface OrderItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.microservices.entity.OrderItem;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
 }
